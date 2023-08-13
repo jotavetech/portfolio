1f25ffae5e38e07c943827c23d23328eae11f51a
jaoincode <ojoaogugel@gmail.com>
2023-08-13 17:11:30 -0300

    back to top button & aria labels

---
+++ assets/js/back-top.js
@@ -0,0 +1,19 @@
+const backButton = document.querySelector("#back-top");
+
+backButton.addEventListener("click", () => backToTop());
+
+const backToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+window.onscroll = () => showButton();
+
+const showButton = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 0) {
+    backButton.style.opacity = "100%";
+    return;
+  }
+
+  backButton.style.opacity = "0%";
+};
